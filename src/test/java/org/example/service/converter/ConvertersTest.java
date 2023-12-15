c75876d790bb3abacb40e1890e8df2be45efcf8d
Darov49 <DmitryuSmolev@gmail.com>
2023-12-15 19:22:09 +0300

    Изменен доступ к ресурсам в тестах

--- src/test/java/org/example/service/converter/ConvertersTest.java
+++ src/test/java/org/example/service/converter/ConvertersTest.java
@@ -5,37 +5,56 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConvertersTest {
     @Test
     void testXmlToJsonConvert(@TempDir Path tempDir) throws Exception {
-        Path outputPath = tempDir.resolve("result.json");
-        ConversionService.convert("src/test/resources/data.xml", outputPath.toString());
+        String xmlResourcePath = "data.xml";
+        try (var xmlInputStream = getClass().getClassLoader().getResourceAsStream(xmlResourcePath)) {
+            // Содержимое входного файла копируется во временный файл - вся последующая работа будет именно с ним
+            Path tempFile = tempDir.resolve("temp.xml");
+            Files.copy(xmlInputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
 
-        Path expectedFile = Paths.get("src/test/resources/data.json");
+            Path outputPath = tempDir.resolve("result.json");
+            ConversionService.convert(tempFile.toString(), outputPath.toString());
 
-        String inputFileContent = Files.readString(expectedFile).replaceAll("\\s", "");
-        String outputFileContent = Files.readString(outputPath).replaceAll("\\s", "");
+            String jsonResourcePath = "data.json";
+            Path expectedFile = Paths.get(getClass().getClassLoader().getResource(jsonResourcePath).toURI());
 
-        assertEquals(inputFileContent, outputFileContent);
+            String inputFileContent = Files.readString(expectedFile).replaceAll("\\s", "");
+            String outputFileContent = Files.readString(outputPath).replaceAll("\\s", "");
+
+            assertEquals(inputFileContent, outputFileContent);
+        }
     }
 
     @Test
     void testJsonToXmlConvert(@TempDir Path tempDir) throws Exception {
-        Path outputPath = tempDir.resolve("result.xml");
-        ConversionService.convert("src/test/resources/data.json", outputPath.toString());
+        String jsonResourcePath = "data.json";
+        try (var jsonInputStream = getClass().getClassLoader().getResourceAsStream(jsonResourcePath)) {
+            // Содержимое входного файла копируется во временный файл - вся последующая работа будет именно с ним
+            Path tempFile = tempDir.resolve("temp.json");
+            Files.copy(jsonInputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
 
-        Path expectedFile = Paths.get("src/test/resources/data.xml");
+            Path outputPath = tempDir.resolve("result.xml");
+            ConversionService.convert(tempFile.toString(), outputPath.toString());
 
-        String inputFileContent = Files.readString(expectedFile).trim().replaceAll("\\s", "");
-        String outputFileContent = Files.readString(outputPath).trim().replaceAll("\\s", "");
+            String xmlResourcePath = "data.xml";
+            Path expectedFile = Paths.get(getClass().getClassLoader().getResource(xmlResourcePath).toURI());
 
-        assertEquals(inputFileContent, outputFileContent);
+            String inputFileContent = Files.readString(expectedFile).trim().replaceAll("\\s", "");
+            String outputFileContent = Files.readString(outputPath).trim().replaceAll("\\s", "");
+
+            assertEquals(inputFileContent, outputFileContent);
+        }
     }
 
 }
